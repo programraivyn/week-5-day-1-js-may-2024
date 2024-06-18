6d158fc8789f45d4bbc82b0b500ee1f0768de862
programraivyn <raivynbates@gmail.com>
2024-06-17 20:55:43 -0500

    Create favesports.js

---
+++ favesports.js
@@ -0,0 +1,9 @@
+
+function sportsAdjuster() {
+  myFSHeader.innerHTML = "Basketball is the best!";
+  myFSPara.innerHTML = "I love the thrill of basketball! I love the sound of the sneakers screeching against the court.";
+
+}
+
+myFSButton.onclick = sportsAdjuster;
+  
